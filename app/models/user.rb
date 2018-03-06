2e555c594fe4d3f216cc0529ede6d79c9b16e33a
satoshi.tamura <ec2-user@ip-172-31-20-201.ec2.internal>
2018-03-06 07:12:29 +0000

    timeline

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
   has_secure_password
   
   has_many :microposts
+  
+   def feed_microposts
+    Micropost.where(user_id: self.following_ids + [self.id])
+  end
 end
