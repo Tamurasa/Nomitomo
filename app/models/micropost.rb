c227a70220c6e78c9dc65aad9a2635171593bf85
satoshi.tamura <ec2-user@ip-172-31-20-201.ec2.internal>
2018-03-22 09:23:09 +0000

    修正

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,7 +4,7 @@ class Micropost < ApplicationRecord
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 255 }
   
-  has_many :favorites
+  has_many :favorites, :dependent => :delete_all
 end
 
 
